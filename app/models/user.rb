57be253d2a8a9e00d9931409e847cf9fc727af50
supersakana <zacwilliamson@icloud.com>
2022-08-17 14:39:34 -0600

    added assosiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+
     validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
     validates :email, presence: true, uniqueness: true #, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
