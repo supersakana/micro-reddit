0190d8e8f2628a338079a3ca7657546b8ac078ca
supersakana <zacwilliamson@icloud.com>
2022-08-17 16:06:21 -0600

    finished creating comment model with migration and validations finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :comment
 
     validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
     validates :email, presence: true, uniqueness: true #, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true, length: { in: 6..25 }
 end
